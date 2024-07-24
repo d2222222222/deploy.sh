9a5e784fb4b620aca657e4a39a42037c8fb296bd
d2222222222 <167027165+d2222222222@users.noreply.github.com>
2024-07-24 16:36:55 +0000

    Add files via upload

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
-sudo pacman -S lsd yazi neofetch neovim
+cd
+sudo pacman -S lsd yazi neofetch neovim zip unzip firefox wget
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Hack.zip
+unzip Hack.zip -d Hack
+mkdir -p /usr/local/share/fonts
+sudo mv Hack/*.ttf /usr/local/share/fonts/
+sudo fc-cache -f -v
+cd
 rm -rf ~/.config/nvim/
 git clone https://github.com/LazyVim/starter ~/.config/nvim/
 git clone https://github.com/d2222222222/deploy/
@@ -10,7 +17,8 @@ mv config ~/.config/i3/config
 mv .Xresources ~/.Xresources
 mv init.lua ~/.config/nvim/init.lua
 cd
-rm -rf deploy.sh
+rm -rf deploy
+rm -rf Hack.zip
 #git clone https://github.com/d2222222222/yayinstall
 #cd yayinstall
 #bash yayinstall
