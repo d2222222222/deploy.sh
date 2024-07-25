ace0881e389fec8e7685a1d92a1b5623d7cc2696
d2222222222 <167027165+d2222222222@users.noreply.github.com>
2024-07-25 02:41:34 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
-cd
-sudo pacman -S lsd yazi neofetch neovim zip unzip firefox wget
+echo "yazi is your file manager,when you are in yay downloads you need to do a for all packages and n for showing diffs read what comes up so u dont fuck it.pfetch instead of neofetch,will reboot at the end, script starts in 30 seconds."
+sleep 30
+sudo pacman -S lsd yazi neovim zip unzip firefox wget
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Hack.zip
 unzip Hack.zip -d Hack
 mkdir -p /usr/local/share/fonts
@@ -19,7 +20,13 @@ mv init.lua ~/.config/nvim/init.lua
 cd
 rm -rf deploy
 rm -rf Hack.zip
-#git clone https://github.com/d2222222222/yayinstall
-#cd yayinstall
-#bash yayinstall
-#cd
+git clone https://github.com/d2222222222/yayinstall
+cd yayinstall
+bash yayinstall
+cd
+yay -S pfetch
+yay -S pipes.sh
+clear
+pfetch
+sleep 5
+sudo reboot
