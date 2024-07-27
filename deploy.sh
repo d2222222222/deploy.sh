bbf6d0fd374d8e403b4c7327b175370252395679
d2222222222 <167027165+d2222222222@users.noreply.github.com>
2024-07-27 23:32:43 +0000

    Add files via upload

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
-echo "be prepared to read you have 30 seconds"
-sleep 5
-echo "yazi is your file manager,you have firefox,htop,c and python.my custom neovim,i3 and Xterm complete with a font that has icons for stuff like showing filetypes in yazi.when you are in yay downloads you need to do A for witch packages packages and N for showing diffs read what comes up so u dont fuck it.pfetch instead of neofetch,will reboot at the end.."
-sleep 30
+echo "After the system reboots there is a file named helpme.Open it with lolcat helpme."
+sleep 10
 clear
-sudo pacman -S lsd yazi neovim zip unzip firefox wget htop gcc python3 
+sudo pacman -Syu
+sudo pacman -S lsd yazi neovim vi zip unzip firefox wget htop gcc python3 lolcat
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Hack.zip
 unzip Hack.zip -d Hack
 mkdir -p /usr/local/share/fonts
@@ -20,15 +19,17 @@ cd deploy
 mv config ~/.config/i3/config
 mv .Xresources ~/.Xresources
 mv init.lua ~/.config/nvim/init.lua
+mv .bashrc ~/.bashrc
 cd
 rm -rf deploy
 rm -rf Hack.zip
-git clone https://github.com/d2222222222/yay-installer
-cd yay-installer
+git clone https://github.com/d2222222222/yayinstall
+cd yayinstall
 bash yayinstall
 cd
-yay -S pfetch
-yay -S pipes.sh
+yay -S pfetch pipes.sh tgpt
+xrdb -merge ~/.Xresources
+source ~/.bashrc
 clear
 pfetch
 sleep 5
